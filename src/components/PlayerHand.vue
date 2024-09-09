1084fe9458f9cda35cf15b117512103bae8fe455
Joseph Lionarons <lionsamurai@gmail.com>
2024-09-09 16:45:09 -0400

    create player hand component

---
+++ src/components/PlayerHand.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+const numCards = ref(5);
+</script>
+
+<template>
+  <div id="player-hand">
+    <p>Player Hand placeholder</p>
+    <p>The player's hand contains {{ numCards }} cards.</p>
+  </div>
+</template>
+
+<style scoped>
+#player-hand {
+  position: absolute;
+  text-align: center;
+  width: inherit;
+  bottom: 10px;
+}
+</style>
