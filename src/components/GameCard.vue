6174d79e2e502ca524241b274f5ffbb6c82cacb2
Joseph Lionarons <lionsamurai@gmail.com>
2024-09-10 11:55:42 -0400

    creature card setup

---
+++ src/components/GameCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+  petName: {
+    default: "Bananakeet",
+    type: String
+  },
+  cardType: {
+    type: String,
+    required: true
+  }
+});
+
+function getCreatureImageUrl(petName) {
+  return require('../assets/cards/creatures/' + petName + '.jpg');
+}
+
+const imgUrl = props.cardType === "creature" ? getCreatureImageUrl(props.petName) : "TODO: equipment type logic here";
+</script>
+
+<template>
+  <img class="card" :src="imgUrl">
+</template>
+
+<style scoped>
+.card {
+  margin: 2px;
+  width: 10%;
+}
+</style>
--- src/components/PlayerHand.vue
+++ src/components/PlayerHand.vue
@@ -1,14 +1,43 @@
 <script setup>
 import { ref } from 'vue';
+import GameCard from './GameCard.vue';
+
 const numCards = ref(5);
+const cardIdRef = ref(6);
+
+const cardNames = ["AppleBug", "Babysnakes", "Bananakeet", "Blobbyfish", "PippyChicken", "PocketBat", "SaladSlug", "Squiddy", "StinkMink", "TurtleBunny"];
+
+const playerHand = ref([
+  { petName: "Bananakeet", type: "creature", id: 1 },
+  { petName: "Babysnakes", type: "creature", id: 2 },
+  { petName: "AppleBug", type: "creature", id: 3 },
+  { petName: "PocketBat", type: "creature", id: 4 },
+  { petName: "TurtleBunny", type: "creature", id: 5 }
+]);
+
+function drawCreatureCard() {
+  const newCard = { petName: generateRandomCreatureCard(), type: "creature", id: cardIdRef.value++ };
+  numCards.value++;
+  playerHand.value.push(newCard);
+}
+
+function discardLastCard() {
+  numCards.value--;
+  playerHand.value.pop();
+}
+
+function generateRandomCreatureCard() {
+  const randomIdx = Math.floor(Math.random() * cardNames.length)
+  return cardNames[randomIdx];
+}
 </script>
 
 <template>
   <div id="player-hand">
-    <p>Player Hand placeholder</p>
-    <button @click="numCards--">Discard</button>
-    <button @click="numCards++">Draw</button>
+    <button @click="discardLastCard">Discard</button>
+    <button @click="drawCreatureCard">Draw</button>
     <p>The player's hand contains {{ numCards }} cards.</p>
+    <GameCard v-for="card in playerHand" :key="card.id" :pet-name="card.petName" :card-type="card.type" />
   </div>
 </template>
 
