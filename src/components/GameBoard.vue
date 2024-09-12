ccc0ac5b2e39f368afae3281b1104f9797099e8b
Joseph Lionarons <lionsamurai@gmail.com>
2024-09-12 10:11:03 -0400

    add base game board grid functionality

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,19 +1,42 @@
 <script setup>
+import BoardTile from './BoardTile.vue';
 import { ref } from 'vue';
 
 const numTiles = ref(24);
+
+const tileNames = ["BoulderHills", "CrystalCove", "MistyMountain", "PebbleBeach", "RoaringRapids", "RockslideValley", "SunnyMeadow", "WindyCliffs", "WistfulWoods"];
+
+const boardLayout = [{ tileName: "Start", id: "start-0" }];
+
+for (let idx = 1; idx < numTiles.value - 1; idx++) {
+  const newTileName = tileNames[Math.floor(Math.random() * tileNames.length)];
+  boardLayout.push({ tileName: newTileName, id: `${newTileName}-${idx}` })
+}
+
+boardLayout.push({ tileName: "End", id: "end-23" });
 </script>
 
 <template>
   <div id="game-board">
-    <p>GameBoard placeholder</p>
-    <p>The game will have {{ numTiles }} tiles.</p>
+    <div id="grid">
+      <BoardTile v-for="tile in boardLayout" :key="tile.id" :tile-name="tile.tileName" />
+    </div>
   </div>
 </template>
 
 <style>
 #game-board {
+  display: flex;
+  justify-content: center;
   height: inherit;
   width: inherit;
+}
+
+#grid {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  height: 70vh;
+  width: 70vw;
 }
 </style>
