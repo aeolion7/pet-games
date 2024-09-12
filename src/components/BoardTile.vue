04e35c4626f3a707f12e4f3c9420fb3db89ffadb
Joseph Lionarons <lionsamurai@gmail.com>
2024-09-12 10:10:51 -0400

    create BoardTile component

---
+++ src/components/BoardTile.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+  tileName: String
+});
+
+function getTileImageUrl(tileName) {
+  return require('../assets/tiles/' + tileName + '.png');
+}
+
+const imgUrl = getTileImageUrl(props.tileName);
+</script>
+
+<template>
+  <img class="tile" :src="imgUrl">
+</template>
